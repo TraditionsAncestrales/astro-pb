7c77c9b0e03bedb22386a832a372be7516b5e2ef
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-10-12 21:30:36 +0400

    Update index.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -27,13 +27,17 @@ export const server = {
       if (!listId) return message<Message>(form, "BAD_REQUEST", { status: 400 });
       const subscriberHash = md5(email.toLowerCase());
       mailchimp.setConfig({ apiKey: MAILCHIMP_API_KEY, server: MAILCHIMP_SERVER });
-      // try {
-      //   const { status } = await mailchimp.lists.getListMember(listId, subscriberHash);
-      //   if (status !== "unsubscribed") return message<Message>(form, "CONFLICT", { status: 409 });
-      //   await mailchimp.lists.updateListMember(listId, subscriberHash, { status: "subscribed" });
-      // } catch {
-      //   await mailchimp.lists.addListMember(listId, { email_address: email, status: "subscribed" });
-      // }
+      try {
+        const { status } = await mailchimp.lists.getListMember(listId, subscriberHash);
+        if (status !== "unsubscribed") return message<Message>(form, "CONFLICT", { status: 409 });
+        await mailchimp.lists.updateListMember(listId, subscriberHash, { status: "subscribed" });
+      } catch (error) {
+        try {
+          await mailchimp.lists.addListMember(listId, { email_address: email, status: "subscribed" });
+        } catch (error) {
+          return message<Message>(form, "BAD_REQUEST", { status: 400 });
+        }
+      }
       return message<Message>(form, "SUCCESS");
     },
   }),
